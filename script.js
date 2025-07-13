828a1ae7482501bfa9125442f24c60ee3b0f3db7
Nick Leopold <36464582+LeopoldN@users.noreply.github.com>
2025-07-13 21:33:45 +0200

    V0.2.0

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     particlesJS('particles-js', {
       particles: {
         number: { value: 60, density: { enable: true, value_area: 800 } },
-        color: { value: '#ff4081' },
+        color: { value: '#fc0303' },
         shape: { type: 'circle' },
         opacity: { value: 0.5 }, size: { value: 3 },
         line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.2, width: 1 },
